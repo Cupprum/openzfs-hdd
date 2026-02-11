c7e1dc0f0b3a53967e65cf5cc3f173c8c6eada3b
Cupprum <branisa.samuel@icloud.com>
2026-02-11 23:30:15 +0100

    chore: create groups using the script

--- zfs.sh
+++ zfs.sh
@@ -6,15 +6,32 @@ case "$1" in
   connect)
     sudo zpool import $POOL
     sudo zfs load-key $POOL
-    sudo zfs mount $POOL || exit 1
+    sudo zfs mount -o mountpoint=/Users/x42/openzfs $POOL || exit 1
     echo "- $POOL is now unlocked and mounted."
     ;;
   disconnect)
     sudo zpool export $POOL || exit 1
     echo "- $POOL safely exported and locked."
     ;;
+  setup-group)
+    case "$(uname)" in 
+      Darwin)
+        sudo dscl . create /Groups/zfs-share
+        sudo dscl . create /Groups/zfs-share PrimaryGroupID 2000
+        sudo dscl . append /Groups/zfs-share GroupMembership $USER
+        ;;
+      Linux)
+        sudo groupadd -g 2000 zfs-share
+        sudo usermod -aG zfs-share $USER
+        ;;
+      *)
+        echo "Invalid system: $(uname)"
+        exit 1
+        ;;
+    esac
+    ;;
   *)
-    echo "Usage: $0 {connect|disconnect}"
+    echo "Usage: $0 {connect|disconnect|setup-group}"
     exit 1
     ;;
 esac
